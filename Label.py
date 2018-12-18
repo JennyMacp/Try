77ec0c8b6c92f977bee53794824e6fa3988ae456
Jenny Macp <jennymazanicp@gmail.com>
2018-12-18 10:51:11 +0700

    jen

--- Label.py
+++ Label.py
@@ -2,11 +2,12 @@ import tkinter
 
 jendela = tkinter.Tk()
 
-LabelAnggur = tkinter.Label (jendela, text = "Anggur", fg = "white", bg = "violet")
+LabelAnggu = tkinter.Label (jendela, text = "Anggur", fg = "white", bg = "violet")
 LabelApel = tkinter.Label (jendela, text = "Apel", fg = "white", bg = "red")
 LabelMelon = tkinter.Label (jendela, text = "Melon", fg = "white", bg = "green")
 
-LabelAnggur.pack()
+
+LabelAnggu.pack()
 LabelApel.pack(ipadx = 20, ipady = 10)
 LabelMelon.pack()
 
